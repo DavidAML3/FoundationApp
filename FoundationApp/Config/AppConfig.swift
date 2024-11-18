e4aa2b8dd08000ba145de6937a71666a0548d4ef
DavidAML3 <davidaml@Davids-MacBook-Pro.local>
2024-11-18 15:01:00 -0500

    initial commit

---
+++ FoundationApp/Config/AppConfig.swift
@@ -0,0 +1,12 @@
+//
+//  AppConfig.swift
+//  FoundationApp
+//
+//  Created by David Andres Mejia Lopez on 30/10/24.
+//
+
+import Foundation
+
+struct AppConfig {
+    static let baseUrl = ""
+}
---
+++ FoundationAppTests/FoundationAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  FoundationAppTests.swift
+//  FoundationAppTests
+//
+//  Created by David Andres Mejia Lopez on 30/10/24.
+//
+
+import Testing
+@testable import FoundationApp
+
+struct FoundationAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
